8e6ea58773b5ec8e33cfcb734001216d685966fd
yetone <i@yetone.net>
2015-11-20 10:39:53 +0800

    fix typo

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     keywords=('babel', 'babeljs', 'ES', 'JavaScript',
               'ES2015', 'ES2016', 'ES2017'),
     description='The python binding for babel compiler',
-    url='http://github.com/yetone/bablejs-python',
+    url='http://github.com/yetone/babeljs-python',
     license='MIT License',
     author='yetone',
     author_email='i@yetone.net',
